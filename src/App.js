60e4b9d7182e8daabd4788a2a066a9a1de7cc26c
Stephan Panteleev <stiopaonmac@gmail.com>
2022-06-15 13:43:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { data } from "./data";
 
 function App() {
+  console.log(data.data.map((team) => team));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Premier League Football Teams</h1>
+      <div className="container">
+        {data.data.map((team) => (
+          <div class="box">
+            <div class="imgBox">
+              <img src={team.image} />
+            </div>
+            <div class="content">
+              <h2>
+                <a href={team.url} target="_blank">
+                  {team.name}
+                </a>
+              </h2>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
